8202629c022224ee2fbf6ab5c5769c28f4a2c750
cdis001 <shopingid@naver.com>
2023-04-24 02:02:44 +0900

    UI. Create Footer  - Footer UI 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,60 +7,126 @@ import bagIcon from "public/imgs/bag-icon.svg";
 import heartIcon from "public/imgs/heart-icon.svg";
 import peopleIcon from "public/imgs/people-icon.svg";
 import styles from "@/styles/Home.module.css";
-
-export default function Home() {
-  const MainBanner = styled.div`
+const MainBanner = styled.div`
+  width: 100%;
+  & > img {
     width: 100%;
-    & > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  `;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
-  const Header = styled.div`
-    position: fixed;
+const Header = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  width: 100%;
+  height: 72px;
+  padding: 24px 50px;
+  // background-color: #ccc;
+`;
+
+const MainLogo = styled.h1`
+  font-family: "Poppins", sans-serif;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 0.1em;
+  color: #fff;
+`;
+
+const HeaderButton = styled.a`
+  font-family: "Poppins", sans-serif;
+  font-size: 14px;
+  letter-spacing: -0.02em;
+  color: #fff;
+`;
+
+const HeaderMenuUl = styled.ul`
+  & > li:not(:last-child) {
+    margin-right: 40px;
+  }
+`;
+
+const HeaderIconUl = styled.ul`
+  & > li:not(:last-child) {
+    margin-right: 30px;
+  }
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  border-top: 0.5px solid #9c9e8a;
+  display: flex;
+  justify-content: center;
+`;
+
+const FooterContentBox = styled.div`
+  width: 100%;
+  max-width: 1180px;
+  margin-top: 40px;
+  margin-bottom: 145px;
+  display: flex;
+  justify-content: space-between;
+
+  & > div {
     display: flex;
     justify-content: space-between;
-    box-sizing: border-box;
+    flex-direction: column;
+  }
+`;
+
+const FooterLogoBox = styled.div`
+  flex: 1;
+`;
+const FooterIconUl = styled.ul`
+  & > li {
+    margin: 0;
+  }
+
+  & > li:not(:last-child) {
+    margin-right: 19px;
+  }
+`;
+const SocialMediaIcon = styled.button`
+  width: 21px;
+  height: 21px;
+  background-color: #000;
+  border-radius: 25%;
+`;
+
+const MenuList = styled.div`
+  flex: 2;
+  & strong {
+    font-weight: 400;
+  }
+  & > ul > li {
     width: 100%;
-    height: 72px;
-    padding: 24px 50px;
-    // background-color: #ccc;
-
-    & > h1 {
-      font-family: "Poppins", sans-serif;
-      font-size: 20px;
-      font-weight: 500;
-      letter-spacing: 0.1em;
-      color: #fff;
-    }
-
-    // & > ul > li:not(:last-child) {
-    //   margin-right: 35px;
-    // }
-  `;
-
-  const HeaderButton = styled.a`
+    justify-content: space-between;
+  }
+  & > ul > li > ul {
+    display: flex;
+    flex-direction: column;
     font-family: "Poppins", sans-serif;
+    line-height: 24px;
+  }
+  & > ul > li > ul > li:first-child {
     font-size: 14px;
     letter-spacing: -0.02em;
-    color: #fff;
-  `;
+    margin-bottom: 33px;
+  }
+  & > ul > li > ul > li:not(:first-child) {
+    font-size: 14px;
+    color: #9c9e8a;
+    letter-spacing: -0.02em;
+    font-weight: 300;
+  }
+  & > ul > li > ul > li:nth-child(2) > p {
+    margin-bottom: 24px;
+  }
+`;
 
-  const HeaderMenuUl = styled.ul`
-    & > li:not(:last-child) {
-      margin-right: 40px;
-    }
-  `;
-
-  const HeaderIconUl = styled.ul`
-    & > li:not(:last-child) {
-      margin-right: 30px;
-    }
-  `;
-
-
+export default function Home() {
   return (
     <>
       <Head>
@@ -81,7 +147,7 @@ export default function Home() {
               <HeaderButton>Contacts</HeaderButton>
             </li>
           </HeaderMenuUl>
-          <h1>RATIO25</h1>
+          <MainLogo>RATIO25</MainLogo>
           <HeaderIconUl>
             <li>
               <HeaderButton>
@@ -103,6 +169,103 @@ export default function Home() {
         <MainBanner styles={{ aspectRatio: 16 / 9 }}>
           <Image src={mainBannerImg} alt={"main banner"} />
         </MainBanner>
+        <Footer>
+          <FooterContentBox>
+            <FooterLogoBox>
+              <MainLogo>RATIO25</MainLogo>
+              <FooterIconUl>
+                <li>
+                  <SocialMediaIcon></SocialMediaIcon>
+                </li>
+                <li>
+                  <SocialMediaIcon></SocialMediaIcon>
+                </li>
+                <li>
+                  <SocialMediaIcon></SocialMediaIcon>
+                </li>
+              </FooterIconUl>
+            </FooterLogoBox>
+            <MenuList>
+              <ul>
+                <li>
+                  <ul>
+                    <li>
+                      <h3>SHOP</h3>
+                    </li>
+                    <li>
+                      <a>New</a>
+                    </li>
+                    <li>
+                      <a>Best</a>
+                    </li>
+                    <li>
+                      <a>Men</a>
+                    </li>
+                    <li>
+                      <a>Women</a>
+                    </li>
+                    <li>
+                      <a>Copule</a>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <h3>ABOUT</h3>
+                    </li>
+                    <li>
+                      <a>Brand story</a>
+                    </li>
+                    <li>
+                      <a>Delivery</a>
+                    </li>
+                    <li>
+                      <a>Payment</a>
+                    </li>
+                    <li>
+                      <a>FAQ</a>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <h3>CONTACTS</h3>
+                    </li>
+                    <li>
+                      <p>
+                        <strong>E-mail</strong>
+                        <br />
+                        <span>help@ratio25.com</span>
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        <strong>Bank Info</strong>
+                        <br />
+                        <span>기업 469-045608-01-031</span>
+                      </p>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>&nbsp;</li>
+                    <li>
+                      <p>
+                        <strong>CS Center</strong>
+                        <br />
+                        <span>1661-6398</span>
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        <span>평일 10:00~17:00</span>
+                        <br />
+                        <span>(점심시간 12:00~13:00)</span>
+                      </p>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </MenuList>
+          </FooterContentBox>
+        </Footer>
       </main>
     </>
   );
